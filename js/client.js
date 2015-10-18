a8123538821321f26b74b6e1e87aa195bd0b4a8f
Hal Henke <halhenke@gmail.com>
2015-10-19 10:32:24 +1100

    Basic file loading

---
+++ js/client.js
@@ -0,0 +1,20 @@
+var selector;
+var pixelFixer;
+var reader = new FileReader();
+var image = new Image;
+
+window.onload = function() {
+  selector = document.querySelector('input.imageReader');
+  selector.onchange = function() {
+    if (this.files && this.files[0]) {
+      var imageFile = this.files[0];
+      reader.readAsDataURL(imageFile);
+    }
+  };
+};
+
+reader.onload = function(e) {
+  image.src = e.target.result;
+  image.onload = function() {
+  };
+};
